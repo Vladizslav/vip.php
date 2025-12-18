1d66c3feae0402c4e601773a79e0282c7d6f3e60
Mamura <vladizssslav@mail.ru>
2025-12-19 02:26:21 +0300

    Add files via upload

---
+++ lab6/page2.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+include('savepage.inc.php');
+?>
+<!doctype html>
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Страница 2</title>
+</head>
+<body>
+
+<h1>Страница 2</h1>
+
+<?php
+include('menu.inc.php');
+
+
+include('visited.inc.php');
+?>
+
+</body>
+</html>
---
+++ lab6/page3.php
@@ -0,0 +1,24 @@
+<?php
+
+
+session_start();
+
+include('savepage.inc.php');
+?>
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Страница 3</title>
+</head>
+<body>
+
+<h1>Страница 3</h1>
+
+<?php
+include('menu.inc.php');
+include('visited.inc.php');
+?>
+
+</body>
+</html>
