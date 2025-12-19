19f33458afe0b5f68b4b93127dc9565cd7404211
Mamura <vladizssslav@mail.ru>
2025-12-19 15:52:48 +0300

    Create config.php

---
+++ lab8/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_NAME', 'f1199983_BD');
+define('DB_USER', 'f1199983_BD');
+define('DB_PASSWORD', 'QQx-vzw-n7j-LhA');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+?>
