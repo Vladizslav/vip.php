0e5a32d5d2a4eafd2ce1544f385bce3a687854b6
Mamura <vladizssslav@mail.ru>
2025-12-24 12:59:46 +0300

    Update page2.php

--- lab6/page2.php
+++ lab6/page2.php
@@ -1,27 +1,27 @@
 <?php
-ini_set('session.use_trans_sid', 0);
-session_start();
+ declare(strict_types=1);
 
-include('savepage.inc.php');
+ session_start();
+
+ // Подключаем код для сохранения информации о странице в сессии
+ include 'savepage.inc.php';
 ?>
-<!doctype html>
+<!DOCTYPE html>
+<html lang="ru">
+ <head>
+  <meta charset="UTF-8">
+  <title>Страница 2</title>
+ </head>
+ <body>
 
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Страница 2</title>
-</head>
-<body>
+  <h1>Страница 2</h1>
 
-<h1>Страница 2</h1>
+  <?php
+   include 'menu.inc.php';
 
-<?php
-include('menu.inc.php');
+   // Подключаем код для вывода информации обо всех посещённых страницах
+   include 'visited.inc.php';
+  ?>
 
-
-include('visited.inc.php');
-?>
-
-</body>
-
+ </body>
 </html>
